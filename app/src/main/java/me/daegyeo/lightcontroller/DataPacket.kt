ae0e7bf908e836d8c8b7cc17dad517e21cd4309d
Daegyeom Ha <combbm@gmail.com>
2022-11-24 00:37:48 +0900

    chore: order by alphabet

--- app/src/main/java/me/daegyeo/lightcontroller/DataPacket.kt
+++ app/src/main/java/me/daegyeo/lightcontroller/DataPacket.kt
@@ -1,16 +1,16 @@
 package me.daegyeo.lightcontroller
 
 object DataPacket {
-    val LIGHT_ON = 'O'.code.toByte()
-    val LIGHT_OFF = 'F'.code.toByte()
     val LIGHT_AUTO_ON = 'A'.code.toByte()
-    val LIGHT_AUTO_OFF = 'U'.code.toByte()
-    val FRONT1 = 'F'.code.toByte()
     val BACK1 = 'B'.code.toByte()
-    val FRONT2 = 'G'.code.toByte()
     val BACK2 = 'C'.code.toByte()
+    val FRONT1 = 'F'.code.toByte()
+    val FRONT2 = 'G'.code.toByte()
+    val LIGHT_OFF = 'K'.code.toByte()
     val LEFT = 'L'.code.toByte()
+    val LIGHT_ON = 'O'.code.toByte()
     val RIGHT = 'R'.code.toByte()
+    val LIGHT_AUTO_OFF = 'U'.code.toByte()
     val WHITE = 'W'.code.toByte()
     val RED = 'X'.code.toByte()
     val GREEN = 'Y'.code.toByte()
